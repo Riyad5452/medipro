bfc2dcd7356d9515c2d8440ca721974dbe62cc90
Riyad5452 <98330148+Riyad5452@users.noreply.github.com>
2023-08-19 20:59:53 +0800

    Add files via upload

---
+++ Examblog.js
@@ -0,0 +1,4 @@
+function setting(){
+       document.querySelector('.copyright').innerHTML = "Copyright  @ Examblog Created by <a href="https://www.facebook.com/groups/exambloggas"
+        target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a>"
+    }
