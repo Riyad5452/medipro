0be975d3dd67c487dd6286bf1c6ee7a75be888be
Riyad5452 <98330148+Riyad5452@users.noreply.github.com>
2023-08-19 21:01:49 +0800

    Examblog.js

--- Examblog.js
+++ Examblog.js
@@ -1,4 +1,4 @@
 function setting(){
-       document.querySelector('.copyright').innerHTML = "Copyright  @ Examblog Created by <a href="https://www.facebook.com/groups/exambloggas"
+       document.querySelector('.copyright').innerHTML = "Copyright  @ MediCeram Created by <a href="https://www.facebook.com/groups/Mediceram"
         target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a>"
     }
